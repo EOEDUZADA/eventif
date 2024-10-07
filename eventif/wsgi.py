a2ab55120546821c82bf4fce51e1e5a75667ba64
EOEDUZADA <eduardo.silva@aluno.riogrande.ifrs.edu.br>
2024-10-07 16:46:36 -0300

    realiza inscriçao

--- eventif/wsgi.py
+++ eventif/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
 import os
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventif.settings")
-
 application = Cling(get_wsgi_application())
+
