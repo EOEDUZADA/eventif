79eca2455d72fc4eba1bc295ec7323a60f6ed059
EOEDUZADA <eduardo.silva@aluno.riogrande.ifrs.edu.br>
2024-12-09 21:10:31 -0300

    Finalizando o projeto

--- core/views.py
+++ core/views.py
@@ -3,10 +3,7 @@ from core.models import Speaker
 
 
 def home(request):
-    speakers = [
-        {'name': 'Grace Hopper', 'photo': 'https://abre.ai/hopper-pic'},
-        {'name': 'Alan Turing', 'photo': 'https://abre.ai/turing-pic'}
-    ]
+    speakers = Speaker.objects.all()
     return render(request, 'index.html', {'speakers': speakers})
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
